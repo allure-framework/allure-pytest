69c33899216c8ed26630fb00f5aa6d634e933cf4
pyatil <rusbugtest@gmail.com>
2015-12-22 15:08:40 +0300

    clear test_case when it ends.

--- allure/common.py
+++ allure/common.py
@@ -176,7 +176,7 @@ class AllureImpl(object):
             test.failure = Failure(message=message, trace=trace or '')
 
         self.testsuite.tests.append(test)
-
+        self.test_case = None
         return test
 
     def start_suite(self, name, description=None, title=None, labels=None):
